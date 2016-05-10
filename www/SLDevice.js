d35b642ddfbc7162bf0b5e8fc8c91c65b43f076f
kennywen <Kenny@Kennys-MacBook-Pro.local>
2016-05-10 16:49:19 +0800

    UUID JS Method

--- www/SLDevice.js
+++ www/SLDevice.js
@@ -4,3 +4,6 @@ exports.isJailbroken = function(success, error) {
     exec(success, error, "SLDevice", "isJailbroken", []);
 };
 
+exports.uuidForDevice = function(success, error) {
+    exec(success, error, "SLDevice", "uuidForDevice", []);
+};
